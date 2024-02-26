218b3cca75ffde561254c45267f4805cbd823a19
Anand Kumar <Anand.Kumar@ivycomptech.com>
2024-02-26 11:16:08 +0530

    pushed generated file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppParentComponent } from './app-parent/app-parent.component';
+import { AppChildComponent } from './app-child/app-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppParentComponent,
+    AppChildComponent
   ],
   imports: [
     BrowserModule,
